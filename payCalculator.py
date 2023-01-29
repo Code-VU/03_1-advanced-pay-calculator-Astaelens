1f97e96c4dc1e619fc8666f51fdce7b8d97f27bd
Alex Staelens <Alex.Staelens@vu.com>
2023-01-29 14:05:36 -0600

    added overtime calcultion to pay calculator

--- payCalculator.py
+++ payCalculator.py
@@ -1,6 +1,16 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
+    hrs = input("Enter Hours: ")
+    rate= input("Enter Rate: ")
+    
+    hrs=float(hrs)
+    rate=float(rate)
+
+    if hrs>40:
+        pay= 40*rate+1.5*(hrs-40)*rate
+    else:
+        pay=rate*hrs
+    print("Pay: ",pay)
     
     # end assignment
 
